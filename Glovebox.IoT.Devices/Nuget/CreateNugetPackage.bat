bbbb5af92c3447a9e16f59183ab2dc325a4f76c0
gloveboxes <dglover.au@hotmail.com>
2015-10-29 13:23:12 +1100

    Solution file

--- Glovebox.IoT.Devices/Nuget/CreateNugetPackage.bat
+++ Glovebox.IoT.Devices/Nuget/CreateNugetPackage.bat
@@ -1,9 +1,9 @@
 ﻿rem automate nuget process
 
 mkdir lib\uap10.0
-copy ..\bin\release\Glovebox.Graphics.dll lib\uap10.0 /y
+copy ..\bin\release\Glovebox.IoT.Devices.dll lib\uap10.0 /y
 del *.nupkg
-\data\software\nuget\nuget pack Glovebox.Graphics.dll.nuspec
+\data\software\nuget\nuget pack Glovebox.IoT.Devices.dll.nuspec
 del lib\uap10.0\*.* /q
 rmdir lib /s /q
 
