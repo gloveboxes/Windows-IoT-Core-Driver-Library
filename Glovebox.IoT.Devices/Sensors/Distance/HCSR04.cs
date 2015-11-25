e905cf632db661a94d49e1fd9ab435ba0cb72659
Dave Glover <dglover.au@hotmail.com>
2015-11-26 09:11:16 +1100

    added max distance constructor and method on hc-sr04 sensor

--- Glovebox.IoT.Devices/Sensors/Distance/HCSR04.cs
+++ Glovebox.IoT.Devices/Sensors/Distance/HCSR04.cs
@@ -13,8 +13,8 @@ namespace Glovebox.IoT.Devices.Sensors.Distance
         private GpioPin trig;
         private GpioPin echo;
 
-        private readonly int trig_Pin;
-        private readonly int echo_Pin;
+        private int trig_Pin;
+        private int echo_Pin;
 
 
         static Object deviceLock = new object();
@@ -31,7 +31,23 @@ namespace Glovebox.IoT.Devices.Sensors.Distance
         /// <param name="timeoutMilliseconds">defaults to 20</param>
         public HCSR04(byte trig_Pin, byte echo_Pin, int timeoutMilliseconds=20)
         {
+            Initialise(trig_Pin, echo_Pin, timeoutMilliseconds);
+        }
 
+        /// <summary>
+        /// Initialise ultra sonic distance sensor
+        /// </summary>
+        /// <param name="trig_Pin"></param>
+        /// <param name="echo_Pin"></param>
+        /// <param name="maxDistance">Set Ultra Sonic maximum distance to detect.  This is approximate only.  Based on 34.3 cm per millisecond, 20 degrees C at sea level</param>
+        public HCSR04(byte trig_Pin, byte echo_Pin, Length maxDistance)
+        {
+            int milliSeconds = (int)(maxDistance.Centimeters / 34.3 * 2);
+            Initialise(trig_Pin, echo_Pin, milliSeconds);
+        }
+
+        private void Initialise(byte trig_Pin, byte echo_Pin, int timeoutMilliseconds)
+        {
             this.trig_Pin = trig_Pin;
             this.echo_Pin = echo_Pin;
 
@@ -44,6 +60,17 @@ namespace Glovebox.IoT.Devices.Sensors.Distance
             echo.SetDriveMode(GpioPinDriveMode.Input);
 
             trig.Write(GpioPinValue.Low);
+        }
+
+        /// <summary>
+        /// Set Ultra Sonic maximum distance to detect.  This is approximate only.  Based on 34.3 cm per millisecond, 20 degrees C at sea level
+        /// </summary>
+        /// <param name="maxDistance"></param>
+        /// <returns></returns>
+        public Length GetDistance(Length maxDistance)
+        {
+            int milliSeconds = (int)(maxDistance.Centimeters / 34.3 * 2);
+            return GetDistance(milliSeconds);
         }
 
         public Length GetDistance()
@@ -74,11 +101,10 @@ namespace Glovebox.IoT.Devices.Sensors.Distance
 
                 var time = PulseIn(echo, GpioPinValue.High, timeoutMilliseconds);
 
+                // https://en.wikipedia.org/wiki/Speed_of_sound
                 // speed of sound is 34300 cm per second or 34.3 cm per millisecond
                 // since the sound waves traveled to the obstacle and back to the sensor
-                // I am dividing by 2 to represent travel time to the obstacle
-
-                
+                // I am dividing by 2 to represent travel time to the obstacle                
 
                 return Length.FromCentimeters(time * 34.3 / 2.0); // at 20 degrees at sea level
             }
