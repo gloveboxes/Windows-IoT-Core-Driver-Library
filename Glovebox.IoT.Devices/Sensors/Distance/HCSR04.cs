39da00de8b5c8f27a6907cceea7847284868ccfc
Dave Glover <dglover.au@hotmail.com>
2015-11-25 11:47:43 +1100

    Update to hcsr04 distance sensor.  adds timeout

--- Glovebox.IoT.Devices/Sensors/Distance/HCSR04.cs
+++ Glovebox.IoT.Devices/Sensors/Distance/HCSR04.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Threading.Tasks;
+using UnitsNet;
 using Windows.Devices.Gpio;
 
 namespace Glovebox.IoT.Devices.Sensors.Distance
@@ -16,16 +17,19 @@ namespace Glovebox.IoT.Devices.Sensors.Distance
         private readonly int echo_Pin;
 
 
-        Object deviceLock = new object();
+        static Object deviceLock = new object();
 
         Stopwatch sw = new Stopwatch();
 
-        public HCSR04(byte trig_Pin, byte echo_Pin)
+        public int TimeoutMilliseconds { get; set; } = 20;
+
+        public HCSR04(byte trig_Pin, byte echo_Pin, int timeoutMilliseconds)
         {
 
             this.trig_Pin = trig_Pin;
             this.echo_Pin = echo_Pin;
 
+            TimeoutMilliseconds = timeoutMilliseconds;
 
             trig = gpio.OpenPin(trig_Pin);
             echo = gpio.OpenPin(echo_Pin);
@@ -36,8 +40,17 @@ namespace Glovebox.IoT.Devices.Sensors.Distance
             trig.Write(GpioPinValue.Low);
         }
 
+        public Length GetDistance()
+        {
+            return GetDistance(TimeoutMilliseconds);
+        }
 
-        public bool GetDistanceToObstacle(ref double distance)
+        /// <summary>
+        /// Measures distance in centimeters
+        /// </summary>
+        /// <param name="timeoutMilliseconds">20 milliseconds is enough time to measure up to 3 meters</param>
+        /// <returns></returns>
+        public Length GetDistance(int timeoutMilliseconds=20)
         {
             lock (deviceLock)
             {
@@ -45,39 +58,44 @@ namespace Glovebox.IoT.Devices.Sensors.Distance
                 //http://www.c-sharpcorner.com/UploadFile/167ad2/how-to-use-ultrasonic-sensor-hc-sr04-in-arduino/
                 //http://www.modmypi.com/blog/hc-sr04-ultrasonic-range-sensor-on-the-raspberry-pi
 
-                sw.Reset();                                       // reset the stopwatch
 
                 trig.Write(GpioPinValue.Low);                     // ensure the trigger is off
-                Task.Delay(TimeSpan.FromMilliseconds(2)).Wait();  // wait for the sensor to settle
+                Task.Delay(TimeSpan.FromMilliseconds(1)).Wait();  // wait for the sensor to settle
 
                 trig.Write(GpioPinValue.High);                          // turn on the pulse
                 Task.Delay(TimeSpan.FromMilliseconds(.01)).Wait();      // let the pulse run for 10 microseconds
                 trig.Write(GpioPinValue.Low);                           // turn off the pulse
 
-                sw.Start();                                             //start the stopwatch
-
-                while (echo.Read() == GpioPinValue.Low)
-                {               // wait until the echo starts
-                    if (sw.ElapsedMilliseconds > 500)
-                    {
-                        return false;                                   // if you have waited for more than a second, then there was a failure in the echo
-                    }
-                }
-
-                sw.Restart();           // echo is working properly, so restart the stop watch at zero
-
-                while (echo.Read() == GpioPinValue.High) ;
-
-                sw.Stop();  // stop the stopwatch when the echo stops
+                var time = PulseIn(echo, GpioPinValue.High, timeoutMilliseconds);
 
                 // speed of sound is 34300 cm per second or 34.3 cm per millisecond
                 // since the sound waves traveled to the obstacle and back to the sensor
                 // I am dividing by 2 to represent travel time to the obstacle
-                distance = sw.Elapsed.TotalMilliseconds * 34.3 / 2.0;
 
-
-                return true;
+                return Length.FromCentimeters(time * 34.3 / 2.0); // at 20 degrees at sea level
             }
+        }
+
+        private double PulseIn(GpioPin pin, GpioPinValue value, int timeout)
+        {
+            sw.Restart();
+
+            // Wait for pulse
+            while (sw.ElapsedMilliseconds < timeout && pin.Read() != value) { }
+
+            if (sw.ElapsedMilliseconds >= timeout)
+            {
+                sw.Stop();
+                return 0;
+            }
+            sw.Restart();
+
+            // Wait for pulse end
+            while (sw.ElapsedMilliseconds < timeout && pin.Read() == value) { }
+
+            sw.Stop();
+
+            return sw.ElapsedMilliseconds < timeout ? sw.Elapsed.TotalMilliseconds : 0;
         }
     }
 }
