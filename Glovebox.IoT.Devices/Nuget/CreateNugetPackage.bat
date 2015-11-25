644fda39939e3c9297e35a75f7405f4b72b909d6
Dave Glover <dglover.au@hotmail.com>
2015-11-25 12:50:38 +1100

    nuget fix

--- Glovebox.IoT.Devices/Nuget/CreateNugetPackage.bat
+++ Glovebox.IoT.Devices/Nuget/CreateNugetPackage.bat
@@ -1,11 +1,11 @@
 ﻿rem automate nuget process
 
 mkdir lib\uap10.0
-copy ..\bin\release\Glovebox.IoT.Devices.dll lib\uap10.0 /y
+copy ..\bin\ARM\release\Glovebox.IoT.Devices.dll lib\uap10.0 /y
 del *.nupkg
 \data\software\nuget\nuget pack Glovebox.IoT.Devices.dll.nuspec
 del lib\uap10.0\*.* /q
 rmdir lib /s /q
 
-rem nuget setApiKey Your-API-Key
-rem nuget push YourPackage.nupkg
+rem \data\software\nuget\nuget setApiKey API Key
+rem \data\software\nuget\nuget push YourPackage.nupkg
